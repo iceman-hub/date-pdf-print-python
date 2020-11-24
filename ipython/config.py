120d9955157fa6c488f8b0835582283b57a0bd03
e.degtyareva <SkisabOwEtte>
2020-11-24 13:20:21 +0000

    fixed code style

--- ipython/config.py
+++ ipython/config.py
@@ -1,11 +1,14 @@
+# Документ который не подходит "5fb3ce22e84b1a686b5554fc", 
+# матрица было создана для этого "5fb3ce22e84b1a686b55550c".
+
 image_key = "5fb3ce22e84b1a686b555511"
-# тот документ который не подходит 5fb3ce22e84b1a686b5554fc 
-# матрица было создана для этого 5fb3ce22e84b1a686b55550c
-href="http://api.sherlocktest.kt-team.de/api/pages/" + image_key
+
+href = "http://api.sherlocktest.kt-team.de/api/pages/" + image_key
 image_href = href + "/image/actual"
 ocr_href = href + "/ocr"
 
 image_name = "test.png"
+
 basis = {
     'A': {'x': -26, 'y': 17},
     'B': {'x': 34, 'y': 17},
